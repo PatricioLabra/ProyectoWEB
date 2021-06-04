fc14aeb003043206086a7fe541d510fc80f6d979
Dan Santos <dan.santos@cotalker.com>
2021-06-04 15:11:22 -0400

    Se agrega morgan al backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
+// Settings
 app.set('port', process.env.PORT || 4000);
 
+
+// Middlewares
+app.use(morgan('dev'));
+
+
+// Putting the server in listen
 app.listen(app.get('port'), () => {
   console.log(`Server running on port ${app.get('port')}`);
 });
