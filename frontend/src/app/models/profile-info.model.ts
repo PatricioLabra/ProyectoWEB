d9932fa18d1ea615462ec971e80b174ee68328d0
Dan Santos <dan.santos@cotalker.com>
2021-06-12 19:51:37 -0400

    Se crea el model profile-info

---
+++ frontend/src/app/models/profile-info.model.ts
@@ -0,0 +1,10 @@
+export interface ProfileInfo {
+	nickname: string,
+	names?: string,
+	last_names?: string,
+	rut?: string,
+	region?: string,
+	comuna?: string,
+	direccion?: string,
+	email?: string
+}
