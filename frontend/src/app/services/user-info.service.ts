80fad4eb88d72ea0e67612602b47271b1fcf6ff6
Dan Santos <dan.santos@cotalker.com>
2021-06-12 19:51:58 -0400

    Se agrega la informacion que va a guardar el servicio user-info

--- frontend/src/app/services/user-info.service.ts
+++ frontend/src/app/services/user-info.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { ProfileInfo } from '@models/profile-info.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserInfoService {
 
-  constructor() { }
+  isLogged: boolean;
+  isAdmin: boolean;
+  profileInfo: ProfileInfo;
+
+  constructor() {
+    this.isLogged = false;
+    this.isAdmin = false;
+    this.profileInfo = {
+      nickname: 'Visitant'
+    };
+  }
 }
