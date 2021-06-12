6fe6764b61079fe7b2bd972f24e93b1ca5c66c5e
PatricioLabra <patricio.labra827@gmail.com>
2021-06-11 21:40:09 -0400

    se almacena el usuario ingresado en la BD con su validación

--- backend/src/routes/user/user.controller.ts
+++ backend/src/routes/user/user.controller.ts
@@ -1,7 +1,24 @@
 import { RequestHandler } from "express";
+import User from './user.model';
 
-export const addUser: RequestHandler = (req, res) => {
-	res.send({success: true, type: 'addUser'});
+export const addUser: RequestHandler = async (req, res) => {
+	const userFound = await User.findOne({ nickname: req.body.nickname });
+
+	if ( userFound ) {
+		res.status(301);
+		res.send ({
+			success: false, 
+			type: 'addUser'
+		});		
+	} else {
+		const user = new User(req.body);
+		await user.save()							/*almacenamos el usuario en la BD */
+		res.status(200);
+		res.send ({
+			success: true, 
+			type: 'addUser'
+		});
+	}
 };
 
 export const getUser: RequestHandler = (req, res) => {
