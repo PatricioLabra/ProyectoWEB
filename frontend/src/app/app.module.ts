2f9ebd31ef3b204a15b8d13eab39d78607c7373f
Dan Santos <dan.santos@cotalker.com>
2021-06-12 20:00:53 -0400

    Se genera el componente header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from '@components/app-component/app.component';
 import { TestingComponent } from './components/testing/testing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingComponent
+    TestingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
