c56d44560706014c507ae5b8cb9dc229677f1c80
Dan Santos <dan.santos@cotalker.com>
2021-06-12 20:58:19 -0400

    Se genera el componente form-login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './modules/app-routing.module';
+
 import { AppComponent } from '@components/app-component/app.component';
 import { TestingComponent } from './components/testing/testing.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestingComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
