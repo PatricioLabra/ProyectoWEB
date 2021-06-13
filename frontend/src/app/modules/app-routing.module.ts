864ea1b03a82b59c18b15a12719dce857abe868f
Dan Santos <dan.santos@cotalker.com>
2021-06-12 20:58:40 -0400

    Se configura la ruta del login en el router

--- frontend/src/app/modules/app-routing.module.ts
+++ frontend/src/app/modules/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormLoginComponent } from '@components/form-login/form-login.component';
+
+const routes: Routes = [
+  {
+    path: '', component: FormLoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
