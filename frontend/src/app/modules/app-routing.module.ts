b6da76e98c54f4f716fea35ddc14aa667804d4e5
Dan Santos <dan.santos@cotalker.com>
2021-06-12 21:01:22 -0400

    Se deja testing como ruta por defecto, y se crea la ruta de login

--- frontend/src/app/modules/app-routing.module.ts
+++ frontend/src/app/modules/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FormLoginComponent } from '@components/form-login/form-login.component';
+import { TestingComponent } from '@components/testing/testing.component';
 
 const routes: Routes = [
   {
-    path: '', component: FormLoginComponent
+    path: '', component: TestingComponent
+  },
+  {
+    path: 'login', component: FormLoginComponent
   },
 ];
 
