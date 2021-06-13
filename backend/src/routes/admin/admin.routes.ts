1db350e8ecfb27bc7ee5439466f61d94094b7adf
PatricioLabra <patricio.labra827@gmail.com>
2021-06-13 17:33:39 -0400

    se refactoriza la ruta admin

--- backend/src/routes/admin/admin.routes.ts
+++ backend/src/routes/admin/admin.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
 import * as adminCtrl from './admin.controller';
 
+
 const router = Router();
 
 // Agregar un nuevo admin
-router.post('/admin', adminCtrl.addAdmin);
+router.post('/admin/signup', adminCtrl.signUp);
 
 // Obtener la informacion de un admin
 router.get('/admin/:nick', adminCtrl.getAdmin);
 
-// Validar que existe un admin
-router.get('/admin/valid_nick/:nick', adminCtrl.validAdmin);
-
-// Validar que la contraseña (body) sea valida
-router.post('/admin/valid_pass', adminCtrl.validPass);
+// Inicia sesión
+router.post('/admin/signin', adminCtrl.signIn);
 
 export default router;
