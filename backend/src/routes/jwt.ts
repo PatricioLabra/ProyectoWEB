1817ff1cbc621d19d972ed2df6622253c1c597b3
Dan Santos <dan.santos@cotalker.com>
2021-06-13 13:43:16 -0400

    Se documenta jwt.ts

--- backend/src/routes/jwt.ts
+++ backend/src/routes/jwt.ts
@@ -4,11 +4,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * Obtiene un token usando como payload el id, y como clave una variable de entorno
+ * @param id Id del usuario, sobre el cual se obtendra el token
+ * @returns token del usuario
+ */
 export function signToken(id: any) {
 	const token = jwt.sign({ _id: id }, process.env.SECRET_KEY || 'secret_key');
 	return token;
 }
 
+/**
+ * Middleware que maneja la validacion del token del usuario (solo valida que se mande un token)
+ * @param req Request de la peticion, se espera que tenga el headers authorization
+ * @param res Response, si algo falla con el token, retorna el mensaje de error
+ * @param next Siguiente handler a cargar
+ */
 export const verifyToken: RequestHandler = (req, res, next) => {
 	if (!req.headers.authorization)
 		return res.status(400).send({ success: false, message: 'Headers dont have authorization param' });
