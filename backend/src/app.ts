212b43a4064a6e3f371e804ba41c400cfd8e4e39
Dan Santos <dan.santos@cotalker.com>
2021-06-14 17:15:19 -0400

    Se configura express-fileupload en app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
+import fileUpload, { Options } from 'express-fileupload';
 
 dotenv.config();
 
 // Internal imports
 import indexRoutes from './routes/index.routes';
 
+// Configs variables
 const app = express();
-const corsConfig = {
+const corsConfig: CorsOptions = {
   origin: process.env.ORIGIN_FRONT_IP,
   credentials: true
+};
+const fileUploadConfig: Options = {
+  tempFileDir: './temp'
 }
 
 // Settings
@@ -19,6 +24,7 @@ app.set('port', process.env.PORT || 4000);
 
 
 // Middlewares
+app.use(fileUpload(fileUploadConfig));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
