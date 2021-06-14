6cc3cfee2ab671fe6716609f23d46389a3cb12b5
Dan Santos <dan.santos@cotalker.com>
2021-06-13 20:17:48 -0400

    Se cambia el nombre de algunas variables de profile-info a user-info

--- frontend/src/app/services/user-info.service.ts
+++ frontend/src/app/services/user-info.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { UserInfo } from '@models/profile-info.model';
+import { UserInfo } from '@models/user-info.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +9,22 @@ export class UserInfoService {
 
   isLogged: boolean;
   isAdmin: boolean;
-  profileInfo: UserInfo;
+  userInfo: UserInfo;
 
   constructor() {
     this.isLogged = false;
     this.isAdmin = false;
-    this.profileInfo = { nickname: 'Visitant' };
+    this.userInfo = { nickname: 'Visitant' };
   }
 
-  signInUser(profileInfo: UserInfo, isAdmin: boolean) {
+  signInUser(userInfo: UserInfo, isAdmin: boolean) {
     this.isAdmin = isAdmin;
     this.isLogged = true;
-    this.profileInfo = profileInfo;
+    this.userInfo = userInfo;
   }
 
   signOutUser() {
     this.isLogged = false;
-    this.profileInfo = { nickname: 'Visitant' };
+    this.userInfo = { nickname: 'Visitant' };
   }
 }
