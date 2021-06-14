f56cc148788c30d13e7411c6fce8b64897a8a3f3
Dan Santos <dan.santos@cotalker.com>
2021-06-14 17:25:14 -0400

    se agrega una ruta para subir una imagen

--- backend/src/routes/images/images.routes.ts
+++ backend/src/routes/images/images.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { UploadedFile } from "express-fileupload";
 
 const router = Router();
 
@@ -11,7 +12,17 @@ router.get('/images', (req, res) => {
 });
 
 router.post('/image', (req, res) => {
+	if (!req.files)
+		return res.status(401).send({ succes: false, message: 'No images' });
 
+	// Si llega mas de una imagen, llega como array, sino como objeto
+	const images: UploadedFile | UploadedFile[] = req.files.images;
+
+	// Se parsea para trabajar con un array, independiente de si era 1 o mas de una
+	const imagesArray: UploadedFile[] = (Array.isArray(images)) ? images : [images];
+	
+
+	return res.send('Received');
 });
 
 router.put('/image/:id', (req, res) => {
