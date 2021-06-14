991214567119a55c7be8597724908c9a0e17e26b
Dan Santos <dan.santos@cotalker.com>
2021-06-14 17:46:08 -0400

    Se configura para la subida de archivos, (primera version funcional)

--- backend/src/routes/images/images.routes.ts
+++ backend/src/routes/images/images.routes.ts
@@ -1,5 +1,11 @@
-import { Router } from "express";
-import { UploadedFile } from "express-fileupload";
+import { Router } from 'express';
+import { UploadedFile } from 'express-fileupload';
+import dotenv from 'dotenv';
+import AWS from 'aws-sdk';
+
+dotenv.config();
+
+const s3 = new AWS.S3();
 
 const router = Router();
 
@@ -11,7 +17,7 @@ router.get('/images', (req, res) => {
 
 });
 
-router.post('/image', (req, res) => {
+router.post('/image', async (req, res) => {
 	if (!req.files)
 		return res.status(401).send({ succes: false, message: 'No images' });
 
@@ -20,9 +26,24 @@ router.post('/image', (req, res) => {
 
 	// Se parsea para trabajar con un array, independiente de si era 1 o mas de una
 	const imagesArray: UploadedFile[] = (Array.isArray(images)) ? images : [images];
-	
 
-	return res.send('Received');
+	try {
+		s3.endpoint = new AWS.Endpoint(process.env.AWS_ENDPOINT + '/Peliculas' || '')
+
+		const uploadedImage = await s3.putObject({
+			ACL: 'public-read',
+			Bucket: process.env.AWS_BUCKET_NAME || '',
+			Body: imagesArray[0].data,
+			Key: imagesArray[0].name
+		}).promise();
+
+		console.log(uploadedImage);
+	} catch (error) {
+		console.log(error);
+		return res.status(400).send({ success: false, message: 'Error inesperado', error });
+	}
+	
+	return res.status(200).send({ success: true });
 });
 
 router.put('/image/:id', (req, res) => {
