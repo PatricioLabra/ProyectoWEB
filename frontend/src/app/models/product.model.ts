c6c8d330d1d1633b50e0425d5c37a0a12baaeb08
Dan Santos <dan.santos@cotalker.com>
2021-06-15 13:23:59 -0400

    Se crea el modelo del producto

---
+++ frontend/src/app/models/product.model.ts
@@ -0,0 +1,19 @@
+export interface Product {
+	_id: string,
+	name: string,
+	trademark: string,
+	images_urls: Array<string>,
+	price: number,
+	discount: number,
+	description: number,
+	weight: number,
+	dimensions: {
+			height_cm: number,
+			width_cm: number,
+			length_cm: number
+	},
+	stock: number,
+	calification: number,
+	category: string,
+	subcategories: Array<string>,
+}
