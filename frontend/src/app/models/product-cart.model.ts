82ba85d40f98aeb4792e5edd1933ad61d70c4171
Dan Santos <dan.santos@cotalker.com>
2021-06-15 20:16:14 -0400

    Se crea la clase ProductCart

---
+++ frontend/src/app/models/product-cart.model.ts
@@ -0,0 +1,21 @@
+import { Product } from "./product.model";
+
+export class ProductCart {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  discount: number;
+  image_url: string;
+  stock: number;
+
+  constructor(product: Product, quantity: number = 0) {
+    this._id = product._id;
+    this.name = product.name;
+    this.image_url = product.images_urls[0];
+    this.price = product.price;
+    this.discount = product.discount;
+    this.quantity = quantity;
+    this.stock = product.stock;
+  }
+}
