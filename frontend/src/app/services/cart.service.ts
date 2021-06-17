6557d3f06b37f0853b8d7057b3c7e80574beb704
Dan Santos <dan.santos@cotalker.com>
2021-06-16 20:42:25 -0400

    Se documenta cart-service

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -94,11 +94,18 @@ export class CartService {
   public currentDataCart$ = this._cart.asObservable();
 
   constructor() {
+    // Just for debug, remove later
     const listProducts = this.products_.map((product: Product) => new ProductCart(product, 2));
 
     this._cart.next(listProducts);
   }
 
+  /**
+   * Comprueba si el producto ya esta ingresado, si es asi incrementa en 1 su cantidad,
+   * sino ingresa el producto en el carrito
+   * @param product Producto a insertar al carro
+   * @param quantityToInsert cantidad de productos a insertar
+   */
   insertProduct(product: ProductCart, quantityToInsert: number = 1): void {
     const listProducts = this._cart.getValue();
     const index = this.searchProduct(listProducts, product);
@@ -111,6 +118,12 @@ export class CartService {
     this._cart.next(listProducts);
   }
 
+  /**
+   * Comprueba si el producto esta ingresado, si es asi decrementa en 1 su cantidad,
+   * sino no hace nada. Si la cantidad llega a 0, elimina el producto del carrito.
+   * @param product Producto a insertar al carro
+   * @param quantityToInsert cantidad de productos a insertar
+   */
   removeProduct(product: ProductCart, quantityToRemove: number = 1): void {
     const listProducts = this._cart.getValue();
     const index = this.searchProduct(listProducts, product);
@@ -120,16 +133,25 @@ export class CartService {
 
       if (listProducts[index].quantity <= 0)
         listProducts.splice(index, 1);
-    }
 
-    this._cart.next(listProducts);
+      this._cart.next(listProducts);
+    }
   }
 
+  /**
+   * Elimina todos los elementos del carrito
+   */
   emptyCart(): void {
     const listProducts: Array<ProductCart> = [];
     this._cart.next(listProducts);
   }
 
+  /**
+   * Busca el producto ingresado en la lista productos
+   * @param listProducts Lista de productos del carrito
+   * @param product Producto a buscar
+   * @returns number, indice en donde esta el producto, sino esta devuelve -1
+   */
   searchProduct(listProducts: Array<ProductCart>, product: ProductCart): number {
     return listProducts.findIndex((productCart: ProductCart) => productCart._id == product._id);
   }
