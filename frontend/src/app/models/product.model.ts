248531f0aeadba21a3f1c77398a044057fb7199a
Dan Santos <dan.santos@cotalker.com>
2021-06-17 13:06:06 -0400

    Se eliminan los valores por defecto de la clase Product, ya no son necesarios

--- frontend/src/app/models/product.model.ts
+++ frontend/src/app/models/product.model.ts
@@ -18,22 +18,17 @@ export class Product {
 	subcategories: Array<string>;
 
 	constructor() {
-			this._id = "60c81124c727db35ac704bbb",
-			this.name = "Salvando al Soldado Ryan";
+			this._id = "",
+			this.name = "";
 			this.trademark = "";
-			this.images_urls = ["https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Peliculas/salvar_al_soldado_ryan_2.jpg"];
-			this.price = 9990;
-			this.discount = 0.1;
-			this.description = "Después de desembarcar en Normandía";
-			this.weight = 83.16,
-			this.dimensions = {
-					height_cm: 18.03,
-					width_cm: 1.48,
-					length_cm: 13.76
-			};
-			this.stock = 4;
-			this.calification = 4.9;
-			this.category = "Películas";
-			this.subcategories = ["Acción"]
+			this.images_urls = [];
+			this.price = 0;
+			this.discount = 0;
+			this.description = "";
+			this.weight = 0;
+			this.stock = 0;
+			this.calification = 0;
+			this.category = "";
+			this.subcategories = [];
 	}
 }
