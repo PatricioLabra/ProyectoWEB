4f2a559c56ceff1f9be20b9ee02f8168215d863f
Dan Santos <dan.santos@cotalker.com>
2021-06-16 20:35:31 -0400

    Se implementa observables para el carrito

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,48 +1,136 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ProductCart } from '@models/product-cart.model';
+import { Product } from '@models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  products: Array<ProductCart> = [];
-
-  constructor() { }
-
-  insertProduct(product: ProductCart): boolean {
-    const index: number = this.searchProduct(product);
-    let itChanges: boolean = false;
-
-    if (-1 == index) {
-      this.products.push(product);
-      itChanges = true;
-    } else if (this.products[index].hasDisponibility()) {
-      this.products[index].quantity++;
-      itChanges = true;
+  // Just for debub, remove later
+  products_: Array<Product> = [
+    {
+        "_id": "60c6bf9282cdd039186ae8f3",
+        "name": "Mascarilla Backstreet Boys",
+        "trademark": "",
+        "images_urls": [
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/mascarilla_backstreet_boys_1.jpg",
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/mascarilla_backstreet_boys_2.jpg"
+        ],
+        "price": 4500,
+        "discount": 0.1,
+        "description": "Mascarilla reutilizable con 4 capas protectoras. Construida con algodón y Poliester",
+        "weight": 50,
+        "dimensions": {
+            "height_cm": 8,
+            "width_cm": 0.3,
+            "length_cm": 18
+        },
+        "stock": 20,
+        "calification": 4.8,
+        "category": "Ropa",
+        "subcategories": [
+            "Accesorios",
+            "Hombre",
+            "Mujer"
+        ]
+    },
+    {
+        "_id": "60c6c03682cdd039186ae8f4",
+        "name": "Mascarilla Principe del Rap",
+        "trademark": "",
+        "images_urls": [
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/mascarilla_principe_del_rap_1.jpg",
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/mascarilla_principe_del_rap_2.jpg",
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/mascarilla_principe_del_rap_3.jpg"
+        ],
+        "price": 4500,
+        "discount": 0.15,
+        "description": "Mascarilla reutilizable con 4 capas protectoras. Construida con algodón y Poliester",
+        "weight": 50,
+        "dimensions": {
+            "height_cm": 8,
+            "width_cm": 0.3,
+            "length_cm": 18
+        },
+        "stock": 19,
+        "calification": 4.5,
+        "category": "Ropa",
+        "subcategories": [
+            "Accesorios",
+            "Hombre",
+            "Mujer"
+        ]
+    },
+    {
+        "_id": "60c6c08182cdd039186ae8f5",
+        "name": "Polera Blockbuster",
+        "trademark": "",
+        "images_urls": [
+            "https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Ropa/polera_blockbuster_1.jpg"
+        ],
+        "price": 15000,
+        "discount": 0,
+        "description": "Polera hecha con poliéster y tacto de algodón. Esta polera noventera busca revivir la estética mas pura y geométrica de aquellos años.",
+        "weight": 500,
+        "dimensions": {
+            "height_cm": 72,
+            "width_cm": 1,
+            "length_cm": 54
+        },
+        "stock": 10,
+        "calification": 5,
+        "category": "Ropa",
+        "subcategories": [
+            "Moda",
+            "Hombre",
+            "Mujer"
+        ]
     }
+  ];
 
-    console.log(this.products);
-    return itChanges;
+  private _cart = new BehaviorSubject<Array<ProductCart>>([]);
+  public currentDataCart$ = this._cart.asObservable();
+
+  constructor() {
+    const listProducts = this.products_.map((product: Product) => new ProductCart(product, 2));
+
+    this._cart.next(listProducts);
   }
 
-  removeProduct(product: ProductCart): boolean {
-    const index: number = this.searchProduct(product);
-    let itChanges: boolean = false;
+  insertProduct(product: ProductCart, quantityToInsert: number = 1): void {
+    const listProducts = this._cart.getValue();
+    const index = this.searchProduct(listProducts, product);
+
+    if (index != -1)
+      listProducts[index].increaseQuantity(quantityToInsert);
+    else
+      listProducts.push(product);
+
+    this._cart.next(listProducts);
+  }
+
+  removeProduct(product: ProductCart, quantityToRemove: number = 1): void {
+    const listProducts = this._cart.getValue();
+    const index = this.searchProduct(listProducts, product);
 
     if (index != -1) {
-      this.products[index].decreaseQuantity(1);
-      itChanges = true;
+      listProducts[index].decreaseQuantity(quantityToRemove);
 
-      if (this.products[index].quantity <= 0) {
-        this.products.splice(index, 1);
-      }
+      if (listProducts[index].quantity <= 0)
+        listProducts.splice(index, 1);
     }
-    console.log(this.products);
-    return itChanges;
+
+    this._cart.next(listProducts);
   }
 
-  searchProduct(product: ProductCart): number {
-    return this.products.findIndex((productCart: ProductCart) => productCart._id == product._id);
+  emptyCart(): void {
+    const listProducts: Array<ProductCart> = [];
+    this._cart.next(listProducts);
+  }
+
+  searchProduct(listProducts: Array<ProductCart>, product: ProductCart): number {
+    return listProducts.findIndex((productCart: ProductCart) => productCart._id == product._id);
   }
 }
