5fc2d8e9cee56267bf52cabc4c69a91c95f38f52
Dan Santos <dan.santos@cotalker.com>
2021-06-17 11:41:27 -0400

    Se convierte product.model de interfaz a clase

--- frontend/src/app/models/product.model.ts
+++ frontend/src/app/models/product.model.ts
@@ -1,19 +1,39 @@
-export interface Product {
-	_id: string,
-	name: string,
-	trademark: string,
-	images_urls: Array<string>,
-	price: number,
-	discount: number,
-	description: string,
-	weight: number,
+export class Product {
+	_id: string;
+	name: string;
+	trademark: string;
+	images_urls: Array<string>;
+	price: number;
+	discount: number;
+	description: string;
+	weight: number;
 	dimensions?: {
 			height_cm: number,
 			width_cm: number,
 			length_cm: number
-	},
-	stock: number,
-	calification: number,
-	category: string,
-	subcategories: Array<string>,
+	};
+	stock: number;
+	calification: number;
+	category: string;
+	subcategories: Array<string>;
+
+	constructor() {
+			this._id = "60c81124c727db35ac704bbb",
+			this.name = "Salvando al Soldado Ryan";
+			this.trademark = "";
+			this.images_urls = ["https://tienda-ecommerce.sfo3.digitaloceanspaces.com/Peliculas/salvar_al_soldado_ryan_2.jpg"];
+			this.price = 9990;
+			this.discount = 0.1;
+			this.description = "Después de desembarcar en Normandía";
+			this.weight = 83.16,
+			this.dimensions = {
+					height_cm: 18.03,
+					width_cm: 1.48,
+					length_cm: 13.76
+			};
+			this.stock = 4;
+			this.calification = 4.9;
+			this.category = "Películas";
+			this.subcategories = ["Acción"]
+	}
 }
