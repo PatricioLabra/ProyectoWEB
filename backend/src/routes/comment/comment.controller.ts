e300cf3577f5ac5f626168502e2741d52b9cdcc5
PatricioLabra <patricio.labra827@gmail.com>
2021-06-18 02:16:50 -0400

    se agregan las validaciones de manera escrita

--- backend/src/routes/comment/comment.controller.ts
+++ backend/src/routes/comment/comment.controller.ts
@@ -10,24 +10,34 @@ import { Types } from "mongoose"
  */
 export const addComment: RequestHandler = async (req, res) => {
 
+    //se valida que id_product no sea null 
+    //se valida que el arreglo de comentarios no esté vacío
+    //se valida que el id_product no este registrado
+        //si lo está, se agrega el comentario_ingresado a los comentarios ya almacenados con el id_producto ingresado en la BD
+        //si no está, se agregan los comentarios del producto con el id_product
 }
 
 /**
  * Funcion que maneja la petición de los datos todos los comentarios.
  * @route Get '/comments'
  * @param req Request de la peticion, no espera nada como parámetro
- * @param res Response, retornará la informacion de todos los comentarios si todo sale bien
+ * @param res Response, retornará la informacion de todos los comentarios + la cantidad de los mismos si todo sale bien
  */
 export const getComments: RequestHandler = async (req, res) => {
-
+    //se valida que el id_product no sea null
+    //se valida que el id_product sea objectId
+    //se valida que exista un producto almacenado con sus comentarios
 }
 
 /**
  * Funcion que maneja la petición de las calificaciones de todos los comentarios
  * @route Get '/califications'
  * @param req Request de la peticion, no espera nada como parámetro
- * @param res Response, retornará el promedio de todas las calificaciones + su cantidad
+ * @param res Response, retornará el promedio de todas las calificaciones + su cantidad si todo sale bien
  */
 export const getCalificationComments: RequestHandler = async (req, res) => {
+    //se valida que el id_product no sea null
+    //se valida que el id_product sea objectId
+    //se valida que exista un producto almacenado con sus comentarios
 
 }
--- backend/src/routes/comment/comment.routes.ts
+++ backend/src/routes/comment/comment.routes.ts
@@ -8,9 +8,9 @@ const router = Router();
 router.post('/comment', commentCtrl.addComment);
 
 //Obtener los comentarios de un producto
-router.get ('/comments', commentCtrl.getComments)
+router.get ('/comments/:id_product', commentCtrl.getComments)
 
 //Obtener el promedio de las calificaciones + su cantidad
-router.get ('comments/califications', commentCtrl.getCalificationComments)
+router.get ('comments/califications/:id_product', commentCtrl.getCalificationComments)
 
 export default router;
