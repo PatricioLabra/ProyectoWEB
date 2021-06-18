915ba9a49a32208718173ebefebed0673f95825a
Dan Santos <dan.santos.hd@outlook.com>
2021-06-18 15:40:48 -0400

    Se reemplazan todos los httpClient por api-connection

--- frontend/src/app/components/searched-products-section/searched-products-section.component.ts
+++ frontend/src/app/components/searched-products-section/searched-products-section.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Product } from '@models/product.model';
+import { ApiConnectionService } from '@services/api-connection.service';
 
 @Component({
   selector: 'app-searched-products-section',
@@ -11,10 +11,10 @@ export class SearchedProductsSectionComponent implements OnInit {
 
   products: Array<Product> = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private api: ApiConnectionService) { }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:4000/products/newer/0/10').subscribe((data: any) => {
+    this.api.getNewerProducts(0, 10).subscribe((data: any) => {
       this.products = data.products;
     });
   }
