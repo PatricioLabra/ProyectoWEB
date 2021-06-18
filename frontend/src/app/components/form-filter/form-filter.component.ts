6f58ef6a3a29aff05c3d36a369223c8ca6c8d8bf
Dan Santos <dan.santos.hd@outlook.com>
2021-06-18 18:26:58 -0400

    Se agregan los checkbuttons de subcategorias

--- frontend/src/app/components/form-filter/form-filter.component.ts
+++ frontend/src/app/components/form-filter/form-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryList, Subcategories } from '@models/category-types';
 
 @Component({
   selector: 'app-form-filter',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormFilterComponent implements OnInit {
 
-  constructor() { }
+  CategoryList = CategoryList;
+  Subcategories = Subcategories;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
