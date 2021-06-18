d1faf96d2d3518e60400e84221b9875db60476bf
Dan Santos <dan.santos.hd@outlook.com>
2021-06-18 15:22:54 -0400

    Se implementa las funciones signIn y getUserData

--- frontend/src/app/services/api-connection.service.ts
+++ frontend/src/app/services/api-connection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '@environments/environment';
+import { Profile } from '@models/profile.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,27 @@ export class ApiConnectionService {
 
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders({'content-type': 'application/json'});
+  }
+
+  /**
+   * Valida las credenciales del usuario
+   * @param profile perfile con las credenciales del usuario
+   */
+  signIn(profile: Profile) {
+    const url = this.makeUrl(['user', 'signin']);
+    return this.http.post(url, profile);
+  }
+
+  /**
+   * Obtiene los datos del usuario
+   * @param nickname Nickname del usuario
+   * @param token Token del usuario
+   */
+  getUserData(nickname: string, token: string) {
+    const url = this.makeUrl(['user', nickname]);
+    this.headers = this.setToken(token);
+
+    return this.http.get(url, {headers: this.headers});
   }
 
   /**
