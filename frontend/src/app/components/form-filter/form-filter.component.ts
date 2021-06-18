aa08991249939b022c0b13ab231d4aedb03341db
Dan Santos <dan.santos.hd@outlook.com>
2021-06-18 19:33:49 -0400

    Se implementa una funcion para almacenar las subcategorias seleccionadas

--- frontend/src/app/components/form-filter/form-filter.component.ts
+++ frontend/src/app/components/form-filter/form-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CategoryList, Subcategories } from '@models/category-types';
 
 @Component({
@@ -10,11 +11,37 @@ export class FormFilterComponent {
 
   CategoryList = CategoryList;
   Subcategories = Subcategories;
+  SubcategoriesChecked: Array<string> = [];
 
-  constructor() {
+  filterForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+    this.filterForm = this._fb.group({
+      category: [''],
+      minPrice: [],
+      maxPrice: []
+    });
   }
 
-  filterProducts() {
+  onSubmit() {
+    const data = this.filterForm.getRawValue();
 
+    console.log(data, this.SubcategoriesChecked);
+  }
+
+  /**
+   * Va guardando las subcategorias seleccionadas
+   * @param subcategory Subcategoria seleccionada (o deseleccionada)
+   */
+  onChange(subcategory: string) {
+    const index = this.SubcategoriesChecked.findIndex((sub: string) => sub == subcategory);
+
+    if (index != -1) {
+      // Si la subcategoria ya estaba, significa que se deselecciono, por lo tanto se saca
+      this.SubcategoriesChecked.splice(index, 1);
+    } else {
+      // Sino estaba, significa que se selecciono, por lo tanto se ingresa
+      this.SubcategoriesChecked.push(subcategory);
+    }
   }
 }
