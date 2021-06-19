c5ed117db2c24ae3ed1c33699b990de2c805fd3d
Dan Santos <dan.santos.hd@outlook.com>
2021-06-19 01:05:27 -0400

    Se importan los modulos mat-form-field y mat-input

--- frontend/src/app/modules/angular-material/angular-material.module.ts
+++ frontend/src/app/modules/angular-material/angular-material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   exports: [
-
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class AngularMaterialModule { }
