be9110db8580535e2b56fbe48dc29493ca6eb354
PatricioLabra <patricio.labra827@gmail.com>
2021-06-18 20:00:09 -0400

    se habilitan los verifyToken

--- backend/src/routes/cart/cart.routes.ts
+++ backend/src/routes/cart/cart.routes.ts
@@ -6,12 +6,12 @@ import { verifyToken } from '../jwt';
 const router = Router();
 
 //Agregar un nuevo carrito comprado
-router.post('/cart', /*verifyToken,*/ cartCtrl.addCart);
+router.post('/cart', verifyToken, cartCtrl.addCart);
 
 // Obtener una seccion de los ultimos carritos agregados
-router.get('/carts/newer/:init/:quantity', /*verifyTopken,*/ cartCtrl.getNewerCarts);
+router.get('/carts/newer/:init/:quantity', verifyToken, cartCtrl.getNewerCarts);
 
 //Obtener información de un solo carrito comprado 
-router.get('/cart/:id', /*verifyToken,*/ cartCtrl.getCart);
+router.get('/cart/:id', verifyToken, cartCtrl.getCart);
 
 export default router;
