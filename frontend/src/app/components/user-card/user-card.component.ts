75c77448e9fa297a37cb83185f5474dc9c0a88e4
Dan Santos <dan.santos.hd@outlook.com>
2021-06-19 00:19:10 -0400

    Se crea el componente user-card, para que sea mas facil mostrar los usuarios

--- frontend/src/app/components/user-card/user-card.component.ts
+++ frontend/src/app/components/user-card/user-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserInfo } from '@models/user-info.model';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent {
+
+  @Input()
+  user: UserInfo = { nickname: ''};
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
