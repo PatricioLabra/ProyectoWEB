362dca4ba3d045ad6dca4c2ff856a3853b448f5e
Dan Santos <dan.santos.hd@outlook.com>
2021-06-19 01:32:04 -0400

    Se actualizan los select de form-register

--- frontend/src/app/modules/angular-material/angular-material.module.ts
+++ frontend/src/app/modules/angular-material/angular-material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   exports: [
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatSelectModule,
   ]
 })
 export class AngularMaterialModule { }
