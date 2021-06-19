a191666f4bd413c5ced45d139d91919ba8882a5e
Dan Santos <dan.santos.hd@outlook.com>
2021-06-19 01:41:58 -0400

    Se actualizan los checkbox de las subcategorias de form-filter

--- frontend/src/app/modules/angular-material/angular-material.module.ts
+++ frontend/src/app/modules/angular-material/angular-material.module.ts
@@ -3,6 +3,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   exports: [
@@ -10,6 +12,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
+    MatExpansionModule,
+    MatCheckboxModule,
   ]
 })
 export class AngularMaterialModule { }
