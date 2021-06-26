79e7fb05b1247f75dc10c9d5c161425a4066302e
Dan Santos <dan.santos.hd@outlook.com>
2021-06-26 19:52:21 -0400

    Se modifica search-products para que utilize los queryParams

--- frontend/src/app/components/searched-products-section/searched-products-section.component.ts
+++ frontend/src/app/components/searched-products-section/searched-products-section.component.ts
@@ -12,33 +12,33 @@ import { ApiConnectionService } from '@services/api-connection.service';
 export class SearchedProductsSectionComponent implements OnInit {
 
   products: Array<Product> = [];
-  textSearched: string = '';
   currentSkip: number = 0;
   currentQuantity: number = 9;
   quantityProducts: number = 0;
+  textSearched: string = '';
+  currentFilter: FilterType;
 
   constructor(private api: ApiConnectionService, private route: ActivatedRoute) {
+    this.currentFilter = {
+      text_index: '',
+      category: '',
+      subcategories: [],
+      lower_limit: null,
+      upper_limit: null
+    };
   }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((newQueryParams: any) => {
-      this.textSearched = newQueryParams.text;
+      this.textSearched = newQueryParams.text || '';
+      this.currentFilter.text_index = newQueryParams.text || '';
+      this.currentFilter.category = newQueryParams.category || '';
+      this.currentFilter.lower_limit = newQueryParams.lower_limit || null;
+      this.currentFilter.upper_limit = newQueryParams.upper_limit || null;
+      this.currentFilter.subcategories = newQueryParams.subcategories || [];
+      console.log(this.currentFilter);
 
-      this.updateSearchedProducts();
-    });
-  }
-
-  /**
-   * Obtiene los productos relacionados con la busqueda
-   */
-  updateSearchedProducts() {
-    this.api.getSearchProducts(this.textSearched, this.currentSkip, this.currentQuantity).subscribe((data: any) => {
-      console.log(data);
-      console.log(this.currentSkip);
-      console.log(this.currentQuantity);
-      this.products = data.products;
-      this.quantityProducts = data.quantityProducts;
-      this.currentSkip = 0;
+      this.filterProducts();
     });
   }
 
@@ -46,28 +46,29 @@ export class SearchedProductsSectionComponent implements OnInit {
    * Obtiene productos usando el filtro ingresado
    * @param filter Filtro que se aplicara a los productos para buscarlos
    */
-  filterProducts(filter: FilterType) {
-    filter.text_index = this.textSearched;
+  filterProducts() {
+    const filter = this.currentFilter;
     console.log(filter);
 
     this.api.getFilteredProducts(filter, this.currentSkip, this.currentQuantity).subscribe((data: any) => {
       console.log(data);
       this.products = data.products;
       this.quantityProducts = data.quantityProducts;
+      this.currentSkip = 0;
     });
   }
 
   nextPage() {
     if ((this.currentSkip + this.currentQuantity) < this.quantityProducts) {
       this.currentSkip += this.currentQuantity;
-      this.updateSearchedProducts();
+      this.filterProducts();
     }
   }
 
   prevPage() {
     if ((this.currentSkip - this.currentQuantity) >= 0) {
       this.currentSkip -= this.currentQuantity;
-      this.updateSearchedProducts();
+      this.filterProducts();
     }
   }
 }
