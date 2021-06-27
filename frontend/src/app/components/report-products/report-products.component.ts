f47284d9229f3c232ee1ac22ffadb54d4c51f07c
Dan Santos <dan.santos.hd@outlook.com>
2021-06-26 21:24:13 -0400

    Se cambia el tamaño de una letra

--- frontend/src/app/components/report-products/report-products.component.ts
+++ frontend/src/app/components/report-products/report-products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartBought } from '@models/cart-bought.model';
 import { ApiConnectionService } from '@services/api-connection.service';
+import { UserInfoService } from '@services/user-info.service';
 
 @Component({
   selector: 'app-report-products',
@@ -12,11 +13,11 @@ export class ReportProductsComponent implements OnInit {
   carts: Array<CartBought> = [];
   quantityCarts: number = 0;
 
-  constructor(private api: ApiConnectionService) {
+  constructor(private api: ApiConnectionService, private user: UserInfoService) {
   }
 
   ngOnInit(): void {
-    this.api.getNewerCarts(0, 10).subscribe((res: any) => {
+    this.api.getNewerCarts(this.user.token, 0, 10).subscribe((res: any) => {
       this.carts = res.carts;
       this.quantityCarts = res.quantityCarts;
       console.log(res);
